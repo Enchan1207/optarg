7df0bf306370a6f958db59fd06c7820f5e5793b8
GrapeJuicer <Dev.Grape@outlook.jp>
2021-06-28 11:20:59 +0900

    [add] initial commit

---
+++ optarg.c
@@ -0,0 +1,2 @@
+#include "optarg.h"
+
---
+++ optarg.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// インクルード ---------------------------------------
+#include <getopt.h> // struct option, getopt_long(), opterr, optind
+#include <string.h> // strchr()
+
