e9043c931fba3f5b84c7e37b6b5a89cb1cb75138
GrapeJuicer <Dev.Grape@outlook.jp>
2021-11-04 17:04:37 +0900

    [update] add printHelp and printVersion

--- optarg.h
+++ optarg.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <getopt.h> // struct option, getopt_long(), opterr, optind
 #include <string.h> // strchr()
+#include <stdbool.h>
+#include <wctype.h> // iswalnum()
 
 // 定義 -----------------------------------------------
 
@@ -13,6 +15,17 @@
 
 #define SFREE(ptr) { free(ptr); ptr = NULL; }
 
+#define DEFAULT_USAGE "[OPTION] ..."
+
+/*
+struct docstyle style = {
+    .indent     = "  ",
+    .separator  = " , ",
+    .margin     = "        ",
+    .doc_width  = 40
+};
+*/
+#define DEFAULT_STYLE (struct docstyle){ .indent = "  ", .separator = " , ", .margin = "        ", .doc_width = 40 }
 
 // 構造体 ---------------------------------------------
 
@@ -31,6 +44,15 @@ struct docoption
     int *flag;
     char *help_msg;
 };
+
+struct docstyle
+{
+    char *indent;    // default: "  "
+    char *separator; // default: " , "
+    char *margin;    // default: "        "
+    int doc_width;   // default: 40
+};
+
 // プロトタイプ宣言 -----------------------------------
 
 int optprocess(
@@ -53,3 +75,12 @@ int __generateShortOption(const struct docoption *docopts, size_t docopts_size, 
 int __calShortOptsLength(const struct docoption *docopts, size_t docopts_size);
 int __convertOption(const struct docoption *docopts, struct option **longopts, char **shortopts);
 
+int printHelp(const struct docoption *docopts, const char *progname, const char *usage_arg, struct docstyle style, char *postscript);
+void printVersion(const char *progname, const char *version, const char *postscript);
+void __printHeader(const char *progname, const char *usage_arg);
+int __shortOptionCommandLength(const struct docoption *docopts, int docopts_size);
+int __longOptionCommandLength(const struct docoption *docopts, int docopts_size);
+void __printOptionDocs(char *docs, unsigned int indent, unsigned int min_line_size);
+int __optionCommandLength(const struct docoption *docopts, int docopts_size);
+int __calIndentSize(int slen, int llen, const struct docstyle style);
+int __printOptionCommand(const struct docoption opt, unsigned int slen, unsigned int llen, struct docstyle style);
