e267a1595e66a7197705e179cd327f0a157abb46
GrapeJuicer <Dev.Grape@outlook.jp>
2021-11-04 17:02:49 +0900

    [update] add getopt_flex

--- optarg.h
+++ optarg.h
@@ -1,12 +1,18 @@
 #pragma once
 
 // インクルード ---------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
 #include <getopt.h> // struct option, getopt_long(), opterr, optind
 #include <string.h> // strchr()
 
 // 定義 -----------------------------------------------
 
 #define OPT_END {0, 0, 0, 0}
+#define DOCOPT_END {-1, 0, 0, 0, 0, 0}
+
+#define SFREE(ptr) { free(ptr); ptr = NULL; }
+
 
 // 構造体 ---------------------------------------------
 
@@ -16,6 +22,15 @@ struct optarg
     char *arg;
 };
 
+struct docoption
+{
+    int val;
+    int short_name;
+    char *long_name;
+    int has_arg;
+    int *flag;
+    char *help_msg;
+};
 // プロトタイプ宣言 -----------------------------------
 
 int optprocess(
@@ -26,3 +41,15 @@ int optprocess(
     struct optarg findopts[],      // 見つかったオプションと引数の情報を格納する場所
     int len_findopts               // findoptsの配列の長さ
 );
+
+// prototype
+int getopt_flex(int argc, char **argv, const struct docoption *docopts, struct optarg *findopts, size_t findopts_size);
+int __optSize(const struct docoption *opts);
+int __isEnd(struct docoption opt);
+int __initOpts(char **shortopts, size_t longopts_size, struct option **longopts, size_t shortopts_size);
+void __flushOpts(char *shortopts, struct option *longopts);
+int __generateLongOption(const struct docoption *docopts, size_t docopts_size, struct option *longopts, size_t longopts_size);
+int __generateShortOption(const struct docoption *docopts, size_t docopts_size, char *shortopts, size_t shortopts_size);
+int __calShortOptsLength(const struct docoption *docopts, size_t docopts_size);
+int __convertOption(const struct docoption *docopts, struct option **longopts, char **shortopts);
+
